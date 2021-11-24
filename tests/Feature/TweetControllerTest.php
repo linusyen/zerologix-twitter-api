3ee966fca9801b6975467f42649cdec4b141fa82
linusyen <kenshin1988x@gmail.com>
2021-11-24 22:01:35 +0800

    add test cases for TweetController

--- tests/Feature/TweetControllerTest.php
+++ tests/Feature/TweetControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Oauth;
 use App\Models\Tweet;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -19,6 +20,53 @@ class TweetControllerTest extends TestCase
         Tweet::factory()->count(3)->create();
         $response = $this->get('api/users/1/timeline');
 
+        $response->assertStatus(200);
+        $this->assertCount(3, $response['data']);
+        $this->assertEquals('success', $response['status']);
+    }
+
+    public function testCreate()
+    {
+        $oauth = Oauth::factory()->create();
+        $ts = strtotime('2021-11-20');
+        $request = [
+            'user_id' => $oauth->user_id,
+            'oauth_id' => $oauth->id,
+            'access_token' => hash('sha256', $oauth->token . $ts),
+            'content' => 'test123',
+            'ts' => $ts,
+        ];
+        $response = $this->post('api/tweets', $request);
+
+        $response->assertStatus(200);
+        $this->assertEquals('success', $response['status']);
+    }
+
+
+    public function testCreateTokenFail()
+    {
+        $oauth = Oauth::factory()->create();
+        $ts = strtotime('2021-11-20');
+        $request = [
+            'user_id' => $oauth->user_id,
+            'oauth_id' => $oauth->id,
+            'access_token' => 'fail_token',
+            'content' => 'test123',
+            'ts' => $ts,
+        ];
+        $response = $this->post('api/tweets', $request);
+
+        $response->assertStatus(400);
+        $this->assertEquals('fail', $response['status']);
+    }
+
+
+    public function testGetTweets()
+    {
+        Tweet::factory()->count(3)->create();
+        $response = $this->get('api/users/1/tweets');
+
+        $response->assertStatus(200);
         $this->assertCount(3, $response['data']);
         $this->assertEquals('success', $response['status']);
     }
