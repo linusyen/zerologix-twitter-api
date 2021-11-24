d0da059f4b46ce89117cea5d3f1c8b5eaf5b4fb4
linusyen <kenshin1988x@gmail.com>
2021-11-24 09:37:07 +0800

    adjust config for l5-repository

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        // L5-repository Provides,
+        \App\Providers\RepositoryServiceProvider::class,
     ],
 
     /*
